a66a959c189b2bf6eb7338706fd71f4033a6c842
tahsmith <tahs88@gmail.com>
2019-05-06 09:09:29 +1000

    Add Q-learning

---
+++ tabular/q_learning.py
@@ -0,0 +1,16 @@
+def q_learning(policy, gamma, alpha):
+    def episode_fn(state, step, i, Q):
+        action = policy(Q, i, state)
+        done = False
+        while not done:
+            next_state, reward, done, info = step(action)
+            next_action = policy(Q, i, next_state)
+            td_err = (reward + gamma * Q[next_state, :].max() * (not done)
+                      - Q[state, action])
+            Q[state, action] += alpha(i) * td_err
+            state = next_state
+            action = next_action
+
+        return Q
+
+    return episode_fn
