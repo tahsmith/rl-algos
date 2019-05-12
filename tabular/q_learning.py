e03949c15515a5bf11726376cc6e64215a81bdcf
tahsmith <tahs88@gmail.com>
2019-05-12 12:53:20 +1000

    Clean up

--- tabular/q_learning.py
+++ tabular/q_learning.py
@@ -1,15 +1,13 @@
 def q_learning(policy, gamma, alpha):
     def episode_fn(state, step, i, Q):
-        action = policy(Q, i, state)
         done = False
         while not done:
+            action = policy(Q, i, state)
             next_state, reward, done, info = step(action)
-            next_action = policy(Q, i, next_state)
             td_err = (reward + gamma * Q[next_state, :].max() * (not done)
                       - Q[state, action])
             Q[state, action] += alpha(i) * td_err
             state = next_state
-            action = next_action
 
         return Q
 
