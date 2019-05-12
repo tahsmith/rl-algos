2780cf2a845f51715fd1790dde28ae5f709a1a5d
tahsmith <tahs88@gmail.com>
2019-05-12 12:53:09 +1000

    Fix terminal state issue in dyna-q

--- tabular/dyna_q.py
+++ tabular/dyna_q.py
@@ -6,12 +6,13 @@ def planning(n, Q, R, T, alpha, gamma):
     for _ in range(n):
         visited_states = np.where(np.sum(T, axis=(1, 2)) > 0)[0]
         state = np.random.choice(visited_states)
-        actions_taken = np.where(np.sum(T[state], axis=1) > 0)[0]
+        actions_taken = np.where(np.sum(T[state, :, :], axis=1) > 0)[0]
         action = np.random.choice(actions_taken)
         p = T[state, action, :] / T[state, action, :].sum()
         next_state = np.random.choice(Q.shape[0], p=p)
         reward = R[state, action, next_state]
-        td_err = (reward + gamma * Q[next_state, :].max()
+        done = np.all(T[next_state, :, :] == 0)
+        td_err = (reward + gamma * Q[next_state, :].max() * (not done)
                   - Q[state, action])
         Q[state, action] += alpha * td_err
 
@@ -19,10 +20,10 @@ def planning(n, Q, R, T, alpha, gamma):
 
 
 def dyna_q(policy, gamma, alpha, n_planning):
-    def episode_function(state, step, i, Q, T, R):
-        action = policy(Q, i, state)
+    def episode_function(state, step, i, Q, R, T):
         done = False
         while not done:
+            action = policy(Q, i, state)
             next_state, reward, done, info = step(action)
             T[state, action, next_state] += 1
             r_err = reward - R[state, action, next_state]
@@ -31,10 +32,9 @@ def dyna_q(policy, gamma, alpha, n_planning):
                       - Q[state, action])
             Q[state, action] += alpha(i) * td_err
             state = next_state
-            action = policy(Q, i, next_state)
 
             Q = planning(n_planning, Q, R, T, alpha(i), gamma)
 
-        return Q, T, R
+        return Q, R, T
 
     return episode_function
