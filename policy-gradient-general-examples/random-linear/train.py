02f5984d32e2480a15458a7ef8221c800748ad2a
tahsmith <tahs88@gmail.com>
2019-06-18 19:44:45 +1000

    Abstract the model out a bit

--- policy-gradient-general-examples/random-linear/train.py
+++ policy-gradient-general-examples/random-linear/train.py
@@ -32,28 +32,43 @@ def normal(shape, mean_initial, stddev_initial):
     return [mean, stddev], torch.distributions.Normal(loc=mean, scale=stddev)
 
 
+class Estimator:
+    def __init__(self, n_features):
+        self.m_vars, self.m_dist = normal((1, n_features), 0.0, 1.0)
+        self.b_vars, self.b_dist = normal((1,), 0.0, 1.0)
+
+        self.params = sum([self.m_vars, self.b_vars], [])
+        self.dists = [self.m_dist, self.b_dist]
+
+    def estimate(self, x):
+        m = self.m_dist.sample((1,))[0]
+        b = self.b_dist.sample((1,))[0]
+        y_estimate = x.matmul(m.t()) + b
+
+        return y_estimate, (self.m_dist.log_prob(m).sum()
+                            + self.b_dist.log_prob(x).sum())
+
+    def estimate_mean(self, x):
+        m = self.m_dist.mean
+        b = self.b_dist.mean
+        y_estimate = x.matmul(m.t()) + b
+
+        return y_estimate
+
+
 def train():
     learning_rate = 1e-2
     steps = int(1e6)
     batch_size = 1000
 
-    model = torch.nn.Linear(2, 1)
-
     m_true = torch.tensor([-1., -1.])
     b_true = 1.0
 
-    m_vars, m_dist = normal((1,2), 0.0, 1.0)
-    b_vars, b_dist = normal((1,), 0.0, 1.0)
-
-    dists = [
-        m_dist, b_dist
-    ]
-
-    params = m_vars + b_vars
+    estimator = Estimator(2)
 
     sample_data_1d = sample_data(2)
 
-    opt = torch.optim.Adam(params=params, lr=learning_rate)
+    opt = torch.optim.Adam(params=estimator.params, lr=learning_rate)
 
     for i in range(steps):
         opt.zero_grad()
@@ -64,19 +79,12 @@ def train():
         for _ in range(batch_size):
             x_true, y_true = sample_data_1d(m_true, b_true, 1)
 
-            m_trial = m_dist.sample((1,))[0]
-            b_trial = b_dist.sample((1,))[0]
-
-            model.weight.data = m_trial
-            model.bias.data = b_trial
-
-            y_estimate = model(x_true)
+            y_estimate, log_prob = estimator.estimate(x_true)
 
             loss = loss_fn(y_estimate, y_true)
             loss = loss.detach()
             losses.append(loss)
-            policy_vals.append(m_dist.log_prob(m_trial).sum()
-                               + b_dist.log_prob(b_trial).sum())
+            policy_vals.append(log_prob)
 
         losses = torch.tensor(losses)
         loss_mean = torch.std(losses)
@@ -88,15 +96,12 @@ def train():
 
         if ((i + 1) % 10) == 0:
             test_x, test_y = sample_data_1d(m_true, b_true, batch_size)
-            model.weight.data = m_dist.mean.data
-            model.bias.data = b_dist.mean.data
-
-            test_estimate = model(test_x)
+            test_estimate = estimator.estimate_mean(test_x)
             loss = torch.mean(loss_fn(test_estimate, test_y))
             print('loss', loss)
-            print('grad', m_dist.mean.grad, b_dist.mean.grad)
-            print('mean', m_dist.mean.data, b_dist.mean.data)
-            print('std', m_dist.variance.data, b_dist.variance.data)
+            print('grad', [x.grad for x in estimator.params])
+            print('mean', [x.mean for x in estimator.dists])
+            print('std', [x.variance for x in estimator.dists])
 
         opt.step()
 
