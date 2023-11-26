e522b873ce5230e6616079b607e2da2c1303eb29
Tim Smith <2762951+tahsmith@users.noreply.github.com>
2023-11-26 22:04:45 +0000

    rewrite monte carlo to be more type-y

--- tabular/monte_carlo.py
+++ tabular/monte_carlo.py
@@ -24,3 +24,133 @@ def monte_carlo(policy, alpha):
         return Q
 
     return episode
+
+
+from dataclasses import dataclass
+from typing import Callable
+from random import Random
+import numpy as np
+from functools import partial
+from numpy.typing import NDArray
+
+
+@dataclass
+class Step[TState]:
+    state: TState
+    reward: float
+    done: bool
+
+
+type StepFn[TState, TAction] = Callable[[Random, TState, TAction], Step[TState]]
+type ActionFn[TState, TAction] = Callable[[Random, TState], TAction]
+
+type EncodedState = int
+type EncodedAction = int
+type Array = NDArray[np.float64]
+type StateEncoder[TState] = Callable[[TState], EncodedState]
+type ActionDecoder[TAction] = Callable[[EncodedAction], TAction]
+
+type LearningState[TState, TArray] = tuple[TState, TArray, int]
+type LearningSchedule = Callable[[int], float]
+
+
+def epsilon_greedy_policy_probs(Q: Array, eps: float, state: int):
+    action_size = len(Q[0])
+    probs = np.ones(action_size) * eps / action_size
+    probs[np.argmax(Q[state])] = 1 - eps + eps / action_size
+    return probs
+
+
+def epsilon_greedy(eps: LearningSchedule) -> Callable[[Array, int, int], int]:
+    def policy(Q: Array, i: int, state: int):
+        return np.random.choice(
+            len(Q[state]), p=epsilon_greedy_policy_probs(Q, eps(i), state)
+        )
+
+    return policy
+
+
+type History[Action, State] = list[tuple[Action, Step[State]]]
+
+
+def episode_fn[State, Action](
+    random: Random,
+    initial_state: State,
+    step_fn: StepFn[State, Action],
+    action_fn: ActionFn[State, Action],
+) -> History[Action, State]:
+    state = initial_state
+    done = False
+    history: History[Action, State] = []
+    while not done:
+        action = action_fn(random, state)
+        step = step_fn(random, state, action)
+        history.append((action, step))
+        done = step.done
+        state = step.state
+    return history
+
+
+def tabular_epsilon_greedy[State, Action](
+    eps: float,
+    decoder: ActionDecoder[Action],
+    q: Array,
+    random: Random,
+    state: tuple[State, EncodedState],
+) -> tuple[Action, EncodedAction]:
+    i = 0
+    encoded_action = epsilon_greedy(lambda _: eps)(q, i, state[1])
+    return (decoder(encoded_action), encoded_action)
+
+
+def tabular_step_fn[State, Action](
+    step_fn: StepFn[State, Action],
+    encode: StateEncoder[State],
+    random: Random,
+    state: tuple[State, EncodedState],
+    action: tuple[Action, EncodedAction],
+) -> Step[tuple[State, EncodedState]]:
+    step = step_fn(random, state[0], action[0])
+    return Step(
+        state=(step.state, encode(step.state)), reward=step.reward, done=step.done
+    )
+
+
+def monte_carlo_2[State, Action](
+    random: Random,
+    initial_state: State,
+    trials: int,
+    step_fn: StepFn[State, Action],
+    n_states: int,
+    n_actions: int,
+    encoder: StateEncoder[State],
+    decoder: ActionDecoder[Action],
+):
+    q: Array = np.zeros((n_states, n_actions))
+    encoded_step_fn = partial(tabular_step_fn, step_fn, encoder)
+    encoded_initial_state = (initial_state, encoder(initial_state))
+    for i in range(trials):
+        eps = 1 / (i + 1)
+        policy = partial(tabular_epsilon_greedy, eps, decoder, q)
+        history = episode_fn(random, encoded_initial_state, encoded_step_fn, policy)
+        alpha = 1 / (i + 1)
+        q = monte_carlo_update(alpha, q, encoded_initial_state, history)
+
+    return q
+
+
+def monte_carlo_update[State, Action](
+    alpha: float,
+    q: Array,
+    initial_state: tuple[State, EncodedState],
+    history: History[tuple[Action, EncodedAction], tuple[State, EncodedState]],
+) -> Array:
+    q = q.copy()
+    states = [initial_state[1], *(x[1].state[1] for x in history[:-1])]
+    actions = [x[0][1] for x in history]
+    rewards = [x[1].reward for x in history]
+    future_rewards = np.cumsum(rewards[::-1])[::-1]
+    for state, action, future_reward in zip(states, actions, future_rewards):
+        q[state, action] += alpha * (future_reward - q[state, action])
+
+    return q
---
+++ test/test_monte_carlo_2.py
@@ -0,0 +1,32 @@
+from random import Random
+from typing import Literal
+from tabular.monte_carlo import monte_carlo_2, Step
+
+type CorridorState = Literal["h", "s", "g"]
+type CorridorAction = Literal["l", "r"]
+
+
+def corridor_step_fn(
+    random: Random, state: CorridorState, action: CorridorAction
+) -> Step[CorridorState]:
+    if state == "s":
+        if action == "l":
+            return Step(state="h", reward=0, done=True)
+        if action == "r":
+            return Step(state="g", reward=1, done=True)
+    else:
+        raise ValueError()
+
+
+def test_monte_carlo_corridor():
+    q = monte_carlo_2(
+        Random(),
+        "s",
+        100,
+        corridor_step_fn,
+        3,
+        2,
+        lambda s: ["h", "s", "g"].index(s),
+        lambda a: ["l", "r"][a],
+    )
+    print(q)
